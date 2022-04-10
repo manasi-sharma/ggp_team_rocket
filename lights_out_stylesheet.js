88d76a0c5f3a02b61329f8dd9a90a9731ae8f58f
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-04-09 17:35:41 -0700

    lights out style sheet

---
+++ lights_out_stylesheet.js
@@ -0,0 +1,31 @@
+//==============================================================================
+// lightsout
+//==============================================================================
+
+function renderstate (state)
+ {var table = document.createElement('table');
+  table.setAttribute('cellspacing','0');
+  table.setAttribute('bgcolor','#f4f6f8');
+  table.setAttribute('border','4');
+  for (var i=0; i<4; i++) {makerow(table,i,state)};
+  return table}
+
+function makerow (table,rownum,state)
+ {var row =table.insertRow(rownum);
+  for (var j=0; j<4; j++) {makecell(row,rownum,j,state)};
+  return row}
+
+function makecell (row,rownum,colnum,state)
+ {var cell = row.insertCell(row.cells.length);
+  cell.setAttribute('width','40');
+  cell.setAttribute('height','40');
+  cell.setAttribute('align','center');
+  cell.setAttribute('valign','center');
+  if (compfindp(seq('on',(rownum+1).toString(),(colnum+1).toString()),state,seq()))
+     {cell.innerHTML = '<img src="/ggp/games/lightboard/green.jpg"/>'}
+     else {cell.innerHTML = '<img src="/ggp/games/lightboard/red.jpg"/>'};
+  return cell}
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
