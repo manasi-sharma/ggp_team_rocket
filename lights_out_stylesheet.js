c65d264c472eacac6f1714052d915f12981b7cfb
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-04-09 18:44:10 -0700

    GGP Assignment 2

--- lights_out_stylesheet.js
+++ lights_out_stylesheet.js
@@ -6,7 +6,7 @@ function renderstate (state)
  {var table = document.createElement('table');
   table.setAttribute('cellspacing','0');
   table.setAttribute('bgcolor','#f4f6f8');
-  table.setAttribute('border','4');
+  table.setAttribute('border','2');
   for (var i=0; i<4; i++) {makerow(table,i,state)};
   return table}
 
@@ -22,8 +22,10 @@ function makecell (row,rownum,colnum,state)
   cell.setAttribute('align','center');
   cell.setAttribute('valign','center');
   if (compfindp(seq('on',(rownum+1).toString(),(colnum+1).toString()),state,seq()))
-     {cell.innerHTML = '<img src="/ggp/games/lightboard/green.jpg"/>'}
-     else {cell.innerHTML = '<img src="/ggp/games/lightboard/red.jpg"/>'};
+     {cell.innerHTML = '<img src="http://www.clker.com/cliparts/4/u/l/C/i/P/lime-square-md.png" width="50" height="50"/>'}
+     else {cell.innerHTML = '<img src="http://www.clker.com/cliparts/w/x/m/w/d/D/green-square-md.png" width="50" height="50"/>'};
+  if (compfindp('terminal',state,library))
+    {cell.innerHTML = 'Game over'}
   return cell}
 
 //==============================================================================
