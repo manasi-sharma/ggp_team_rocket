a8d4f16af706223ed0e292c2c937385f24514b55
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-05-28 22:30:07 -0700

    Multithreading partially done

--- multithreading/queenWorker.js
+++ multithreading/queenWorker.js
@@ -1,4 +1,5 @@
 self.addEventListener('message', function(e) {
+   console.log("QUEEN CALLED");
     let count = 0;
     var findSolutions = function(ld, cols, rd, all) {
       let poss = ~(ld | cols | rd) & all;
@@ -13,6 +14,12 @@ self.addEventListener('message', function(e) {
         findSolutions((ld | bit) << 1, cols | bit, (rd | bit) >> 1, all);
       }
     };
-    findSolutions(e.data[0], e.data[1], e.data[2], e.data[3]);
-    self.postMessage(count);
+    // findSolutions(e.data[0], e.data[1], e.data[2], e.data[3]);
+    console.log("QUEEN MAING ARRAY");
+    console.log("QUEEN RETURNING");
+    const { arr } = e;
+    arr[0] = 100;
+    arr[1] = 100;
+    console.log("QUEEN RETURNING AFTER");
+    self.postMessage('count');
   }, false);
